53e14778add24dfb26b7c3aa1aafbc2ea37e863b
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-08-03 19:10:12 +0700

    chore: default opts no in mem

--- lua/vi-spell/init.lua
+++ lua/vi-spell/init.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-M.default_opts = {
+local default_opts = {
 	toggle = {
 		filetypes = {
 			"markdown",
@@ -11,7 +11,7 @@ M.default_opts = {
 }
 
 function M.setup(opts)
-	opts = vim.tbl_deep_extend("force", M.default_opts, opts or {})
+	opts = vim.tbl_deep_extend("force", default_opts, opts or {})
 	local group = vim.api.nvim_create_augroup("vi-spell", {})
 	vim.api.nvim_create_autocmd("FileType", {
 		pattern = opts.toggle.filetypes,
