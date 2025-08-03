9cf60d1152bc53e7e5b7a2bf24145199dba6b523
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-08-03 18:26:45 +0700

    feat: gave up, before changing to static one

--- build.lua
+++ build.lua
@@ -1,5 +1,4 @@
-local total = 0
-local completed = 0
+local root = vim.fn.fnamemodify(debug.getinfo(1, "S").source:sub(2), ":p:h")
 
 ---@type ViSpell.Opts
 local default_opts = {
@@ -12,66 +11,76 @@ local opts = vim.tbl_deep_extend("force", default_opts, vim.g.vi_spell_opts or {
 ---@type table<string, string>
 local lists = opts.use_default
 		and {
-			["ibus-bamboo"] = "https://raw.githubusercontent.com/BambooEngine/ibus-bamboo/refs/heads/master/data/vietnamese.cm.dict",
-			["hungspell-vi"] = "https://raw.githubusercontent.com/1ec5/hunspell-vi/refs/heads/main/dictionaries/vi-DauMoi.dic",
+			["vi_VN1.dic"] = "https://raw.githubusercontent.com/BambooEngine/ibus-bamboo/refs/heads/master/data/vietnamese.cm.dict",
+			["vi_VN2.dic"] = "https://raw.githubusercontent.com/1ec5/hunspell-vi/refs/heads/main/dictionaries/vi-DauMoi.dic",
 		}
 	or {}
 if opts.extra then
 	vim.list_extend(lists, opts.extra)
 end
 
-total = #lists
-
-local function build_spell_with_neovim()
-	local files = table.concat(vim.tbl_keys(lists), " ")
-	local cmd = {
-		"nvim",
-		"-u",
-		"NONE",
-		"--headless",
-		"-c",
-		string.format("mkspell! ./spell/vi %s", files),
-		"-c",
-		"qa",
-	}
-	vim.fn.jobstart(cmd, {
-		on_exit = function(_, code, _)
-			vim.schedule(function()
-				if code == 0 then
-					vim.notify("Build Vietnamese spell succesfully!")
-				else
-					vim.notify("Build Vietnamese spell fail!", vim.log.levels.ERROR)
-				end
-			end)
-		end,
-	})
+---@param path string
+local function mkdir(path)
+	path = vim.fs.joinpath(root, path)
+	local stat = vim.uv.fs_stat(path)
+	if not stat then
+		vim.uv.fs_mkdir(path, 493)
+	end
 end
 
----@type fun(err?: string, response?: { body: boolean })
-local function download_callback(err, response)
-	completed = completed + 1
-	if err then
-		vim.schedule(function()
-			vim.notify(err, vim.log.levels.ERROR)
-		end)
+---@param path string
+local function rmdir(path)
+	path = vim.fs.joinpath(root, path)
+	local stat = vim.uv.fs_stat(path)
+	if stat then
+		vim.uv.fs_rmdir(path)
 	end
-	-- if response.body == false then
-	-- 	vim.schedule(function()
-	-- 		coroutine.yield({ "Create a downloaded spell file failed", vim.log.levels.ERROR })
-	-- 	end)
-	-- end
-	if completed == total then
-		build_spell_with_neovim()
-	end
+end
+
+local function mkspell(names)
+	local files = table.concat(
+		vim.tbl_map(function(name)
+			return vim.fs.joinpath(root, string.format("word_lists/%s", name))
+		end, names),
+		" "
+	)
+	local cmd = string.format("mkspell! %s %s", vim.fs.joinpath(root, "spell/vi"), files)
+	vim.print(cmd)
+	vim.cmd(cmd)
 end
 
 ---@param name string
 ---@param url string
 local function download(name, url)
-	local outpath = string.format("word_lists/%s", name)
-	vim.net.request(url, { outpath = outpath }, download_callback)
+	local outpath = vim.fs.joinpath(root, string.format("word_lists/%s", name))
+	local result = vim.system({
+		"curl",
+		"-fLo",
+		outpath,
+		"--create-dirs",
+		url,
+	}, { text = true }):wait()
+
+	if result.code == 0 then
+		return true
+	else
+		vim.print("Download failed:", name, "code:", result.code, "stderr:", result.stderr)
+		return false
+	end
 end
 
-for name, url in pairs(lists) do
-	download(name, url)
+local function main()
+	mkdir("word_lists")
+	local downloaded = {} ---@type string[]
+	for name, url in pairs(lists) do
+		local result = download(name, url) ---@cast result boolean
+		if result then
+			downloaded[#downloaded + 1] = name
+		end
+	end
+	vim.print(downloaded)
+	mkspell(downloaded)
+	rmdir("word_lists")
 end
+
+main()
